8d3652b6d4f9379d0aee056e2b69729e587403e5
leonardofalmeida <ferreiraleoalmeida@gmail.com>
2019-11-02 21:27:04 -0300

    Recebe envia a props do index da lista para o card

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,7 +3,7 @@ import { Container } from './styles';
 import { MdAdd } from 'react-icons/md';
 import Card from '../Card';
 
-export default function List({ data }) {
+export default function List({ data, index: listIndex }) {
   return (
     <Container done={data.done}>
       <header>
@@ -16,7 +16,7 @@ export default function List({ data }) {
       </header>
       <ul>
         {data.cards.map((card, index) =>
-          <Card key={card.id} data={card} index={index} />
+          <Card key={card.id} data={card} index={index} listIndex={listIndex} />
         )}
 
       </ul>
