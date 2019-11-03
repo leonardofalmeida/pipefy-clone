270aa2c86d05a0652bb0ae0993ac87f1640b27e8
leonardofalmeida <ferreiraleoalmeida@gmail.com>
2019-11-02 21:25:11 -0300

    Adiciona metodo para mover cards

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import produce from 'immer';
+
 import { Container } from './styles';
+
 import List from '../List';
+import BoardContext from './context';
 
 import { loadLists } from '../services/api';
 
-const lists = loadLists();
+const data = loadLists();
 
 export default function Board() {
+  const [lists, setLists] = useState(data);
+
+  function move(fromList, from, to) {
+    // Com o produce não mudamos nosso estado, então ele permanece imutável
+    // O draft é uma cópia de lists, sempre o draft sofre uma alteração
+    // ele computa automaticamente no lists
+    setLists(produce(lists, draft => {
+      const dragged = draft[fromList].cards[from];
+
+      //Primeiro remove o item da lista e depois o recoloca no novo lugar
+      draft[fromList].cards.splice(from, 1);
+      draft[fromList].cards.splice(to, 0, dragged);
+    }))
+  }
+
   return (
-    <Container>
-      {lists.map(list => <List key={list.title} data={list} />)}
-    </Container>
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) =>
+          <List key={list.title} index={index} data={list} />
+        )}
+      </Container>
+    </BoardContext.Provider>
   );
 }
